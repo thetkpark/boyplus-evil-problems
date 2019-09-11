e3ab225f599d7d6d0d9abfe4e94826f930b00c73
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2019-09-11 23:11:29 +0700

    add testcase for 40-sum-to-n

---
+++ 40-Sum-To-N/Gen.java
@@ -0,0 +1,18 @@
+/**
+ * Gen
+ */
+import java.lang.Math;
+public class Gen {
+    public static void main(String[] args) {
+    double num = Math.random()*(1000000-1+1)+1;
+    System.out.println((int) num);
+
+    
+        // for(int i=0;i<num;i++){
+        //     double n = Math.random()*(100-1+1)+1;
+        //     System.out.print((int) n + " ");
+        //     //System.out.println((int)n);
+        // }
+        
+    }
+}
