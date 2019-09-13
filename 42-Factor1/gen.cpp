e606495597e15d5a7970dbd4e132f3cbb2dd5fa6
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2019-09-13 11:11:46 +0700

    add factor#1 problem

---
+++ 42-Factor1/gen.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+int main(){
+	srand(time(NULL));
+	int n = rand()%(100000-2+1)+2;
+	printf("%d\n",n);
+    return 0;
+}
+/*
+2^3 x 3^1
+
+
+*/
