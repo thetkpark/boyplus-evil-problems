9ab12b40582c3dc2b3db4ab59272f768f1f3566a
Mr.SETHANANT PIPATPAKORN <62130500230@st.sit.kmutt.ac.th>
2019-12-15 22:14:22 +0700

    [ADD] 84-table

---
+++ 84-Table/Main.java
@@ -0,0 +1,32 @@
+/**
+ * Main
+ */
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int height = sc.nextInt();
+        int length =  sc.nextInt();
+
+        for(int i=0;i<length;i++){
+            System.out.print(' ');
+            System.out.print('_');
+        }
+        System.out.println("");
+        
+        for(int j=0;j<height;j++){
+            
+            for(int i=0;i<=length;i++){
+                System.out.print("|");
+                if(i<length) System.out.print("_");
+            }
+            // for(int i=0;i<length;i++){
+            //     System.out.print(' ');
+            //     System.out.print('_');
+            // }
+            System.out.println("");
+        }
+
+    }
+}
