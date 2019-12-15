dd5a4991185b4629d01bca927b9eb70c86e58d20
Mr.SETHANANT PIPATPAKORN <62130500230@st.sit.kmutt.ac.th>
2019-12-15 19:12:31 +0700

    [ADD] 81 testcase and code

---
+++ 81-Morse-Code#2/Main.java
@@ -0,0 +1,128 @@
+
+/**
+ * Main
+ */
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        //int i = 0;
+        int length = 4;
+        String[] done = str.split(" ");
+        for(int i=0;i<done.length;i++){
+            switch (done[i]) {
+                case ".-":
+                    System.out.print('A');
+                      
+                    break;
+                case "-...":
+                    System.out.print('B');
+                      
+                     
+                    break;
+                case "-.-.":
+                    System.out.print('C');
+                      
+                     
+                    break;
+                case "-..":
+                    System.out.print('D');
+                       
+                    break;
+                case ".":
+                    System.out.print('E');
+                       
+                    break;
+                case "..-.":
+                    System.out.print('F');
+                       
+                    break;
+                case "--.":
+                    System.out.print('G');
+                       
+                    break;
+                case "....":
+                    System.out.print('H');
+                       
+                    break;
+                case "..":
+                    System.out.print('I');
+                       
+                    break;
+                case ".---":
+                    System.out.print('J');
+                       
+                    break;
+                case "-.-":
+                    System.out.print('K');
+                       
+                    break;
+                case ".-..":
+                    System.out.print('L');
+                       
+                    break;
+                case "--":
+                    System.out.print('M');
+                       
+                    break;
+                case "-.":
+                    System.out.print('N');
+                       
+                    break;
+                case "---":
+                    System.out.print('O');
+                       
+                    break;
+                case ".--.":
+                    System.out.print('P');
+                       
+                    break;
+                case "--.-":
+                    System.out.print('Q');
+                       
+                    break;
+                case ".-.":
+                    System.out.print('R');
+                       
+                    break;
+                case "...":
+                    System.out.print('S');
+                       
+                    break;
+                case "-":
+                    System.out.print('T');
+                       
+                    break;
+                case "..-":
+                    System.out.print('U');
+                       
+                    break;
+                case "...-":
+                    System.out.print('V');
+                       
+                    break;
+                case ".--":
+                    System.out.print('W');
+                       
+                    break;
+                case "-..-":
+                    System.out.print('X');
+                       
+                    break;
+                case "-.--":
+                    System.out.print('Y');
+                       
+                    break;
+                case "--..":
+                    System.out.print('Z');
+                    break;
+                default:
+                    System.out.print("error");
+            }
+        }
+    
+    }
+}
