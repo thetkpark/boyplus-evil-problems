11f53b473fc63e106018f4d5b008cd04501d32cf
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2019-12-15 16:55:05 +0700

    Edit Hello do or die solution

--- 73-Hello-Do-or-Die/Main.java
+++ 73-Hello-Do-or-Die/Main.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class Main{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String s1 = sc.next();
-		String s2 = sc.next();
-		System.out.println("Hello, "+s1+" "+s2+".");
+		String name = sc.next();
+		String surname = sc.next();
+		System.out.println("Hello, "+name+" "+surname+".");
 		System.out.println("Welcome to Do or Die exam.");
 	}
 }
